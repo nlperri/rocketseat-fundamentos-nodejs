21831a60ee046bad4ded4eacba37d76b1f5c647b
natalia <lnataliaperri@gmail.com>
2023-03-30 13:42:10 -0300

    Adicionado: Filtrando lista do banco de dados

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from 'node:http'
 import { URL } from 'node:url'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-route-path.js'
 
 // UUID => Unique Universal ID
 
@@ -19,7 +20,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path)
 
-    req.params = { ...routeParams.groups }
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
     return route.handler(req, res)
   }
